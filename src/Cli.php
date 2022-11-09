b42fb25aaf629f134c1b1316b1ff4e57e4244753
nikUnit <kolyaibresi19999@gmail.com>
2022-11-09 09:12:00 +0000

    ex2

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Difference\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function welcome()
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
